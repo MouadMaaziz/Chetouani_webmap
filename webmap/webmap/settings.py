8cd58a20363ada5b48527c93890d5446d386d3e0
MouadMaaziz <morocco8@outlook.fr>
2020-05-04 04:47:53 +0000

    optimizastion

--- webmap/webmap/settings.py
+++ webmap/webmap/settings.py
@@ -166,10 +166,3 @@ LEAFLET_CONFIG = {
 
 
 #GMAIL_CONF
-
-EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS= True
-EMAIL_PORT= 587
-EMAIL_HOST_USER= 'mouad.maaziz@gmail.com'
-EMAIL_HOST_PASSWORD= 
